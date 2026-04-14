f4668ab90a519140e8299e38731eb201c6c48a4b
astri <yohanaastri81@gmail.com>
2026-04-14 13:24:43 +0700

    add key and migrate route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InvitationController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/{slug}', [InvitationController::class, 'show']);
 Route::post('/rsvp', [InvitationController::class, 'storeRsvp']);
+Route::get('/key', function () {
+    Artisan::call('key:generate');
+    return 'key generated';
+});
+Route::get('/migrate', function () {
+    Artisan::call('migrate');
+    return 'migration done';
+});
