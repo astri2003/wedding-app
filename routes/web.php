c2dd2dbfa83febdc5b0445c6ef64a48b62e9a8c5
astri <yohanaastri81@gmail.com>
2026-05-04 12:09:50 +0700

    Move setup route to top to avoid collision with slug route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,19 @@
 <?php
-
+ 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InvitationController;
 use Illuminate\Support\Facades\Artisan;
 
+Route::get('/key', function () {
+    Artisan::call('key:generate');
+    return 'key generated';
+});
+
+Route::get('/setup', function () {
+    Artisan::call('migrate:fresh', ['--force' => true, '--seed' => true]);
+    return 'Database setup (migrate & seed) berhasil!';
+});
+ 
 Route::get('/', function () {
     $invitation = \App\Models\Invitation::first();
     if ($invitation) {
@@ -11,14 +21,6 @@ Route::get('/', function () {
     }
     return 'Belum ada data undangan. Silakan tambahkan data di database.';
 });
-
+ 
 Route::get('/{slug}', [InvitationController::class, 'show']);
 Route::post('/rsvp', [InvitationController::class, 'storeRsvp']);
-Route::get('/key', function () {
-    Artisan::call('key:generate');
-    return 'key generated';
-});
-Route::get('/setup', function () {
-    Artisan::call('migrate:fresh', ['--force' => true, '--seed' => true]);
-    return 'Database setup (migrate & seed) berhasil!';
-});
