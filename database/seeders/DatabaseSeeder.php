89f9fc3ef1fd4f29da328e0609151c14e413659c
astri <yohanaastri81@gmail.com>
2026-05-04 10:25:18 +0700

    Fix 404 by adding root route and seeder for invitation data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,16 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        \App\Models\Invitation::firstOrCreate(
+            ['slug' => 'kevin-chelsea'],
+            [
+                'nama_pria' => 'Kevin',
+                'nama_wanita' => 'Chelsea',
+                'tanggal' => '2026-06-24',
+                'lokasi' => 'Gereja',
+                'maps_link' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.9200016131126!2d98.702882!3d3.6057869999999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3031319de092fdeb%3A0x6909c94625253057!2sGereja%20HKBP%20Pardamean!5e0!3m2!1sid!2sid!4v1777794554657!5m2!1sid!2sid'
+            ]
+        );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InvitationController;
 use Illuminate\Support\Facades\Artisan;
 
+Route::get('/', function () {
+    $invitation = \App\Models\Invitation::first();
+    if ($invitation) {
+        return redirect('/' . $invitation->slug);
+    }
+    return 'Belum ada data undangan. Silakan tambahkan data di database.';
+});
+
 Route::get('/{slug}', [InvitationController::class, 'show']);
 Route::post('/rsvp', [InvitationController::class, 'storeRsvp']);
 Route::get('/key', function () {
